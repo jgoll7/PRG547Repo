89a3bf86f8607dd1985ef61607f71892338d6dba
jacks <jackson.GOLLAN@student.tafesa.edu.au>
2023-11-29 09:42:46 +1030

    Changes made to Person class, created Student class inheriting from Person class. Installed NUnit for testing.

---
+++ TafeSA Enrolment System/ProgramTesting/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stud1 = new Student(1, "Mark Marky", "marky@mail.com", "891819290", "Cert IV Information Technology", new DateTime());
+            Console.WriteLine(stud1);
+        }
+    }
+}
