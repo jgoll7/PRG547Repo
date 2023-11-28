8384cda5b2b5cf648f38f28d27fa826767555cd9
jacks <jackson.GOLLAN@student.tafesa.edu.au>
2023-11-29 07:33:22 +1030

    First commit, library created and basic person class

---
+++ TafeSA Enrolment System/TafeSA Enrolment System/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TafeSA_Enrolment_System
+{
+    abstract class Person
+    {
+        private string name;
+        private string email;
+        private string telNum;
+
+    }
+}
