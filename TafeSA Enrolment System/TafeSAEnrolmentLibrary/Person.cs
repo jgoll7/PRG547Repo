30cafc673b0694ed33dda355288407c2a08de91d
jacks <jackson.GOLLAN@student.tafesa.edu.au>
2023-11-29 10:12:22 +1030

    Changed library from UWP to .NET Framework

---
+++ TafeSA Enrolment System/TafeSAEnrolmentLibrary/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TafeSAEnrolmentLibrary
+{
+    public class Person
+    {
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public String TelNum { get; set; }
+
+        public Person(string name, string email, string telNum)
+        {
+            Name = name;
+            Email = email;
+            TelNum = telNum;
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + " Email: " + Email + " TelNum: " + TelNum; 
+        }
+    }
+}
---
+++ TafeSA Enrolment System/TafeSAEnrolmentLibrary/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TafeSAEnrolmentLibrary
+{
+    public sealed class Student : Person
+    {
+        //assessor methods
+        public String Program { get; set; }
+        public DateTime DateRegistered { get; set; }
+        public int StudentID { get; set; }
+
+        public Student(int studentID, string name, string email, string telNum, string program, DateTime dateRegistered) : base(name, email, telNum)
+        {
+            this.StudentID = studentID;
+            this.Program = program;
+            this.DateRegistered = dateRegistered;
+        }
+
+        public override string ToString()
+        {
+            return "StudentID: "+ StudentID + base.ToString() + "Program: "+ Program + "Date Registered: "+ DateRegistered;
+        }
+    }
+}
