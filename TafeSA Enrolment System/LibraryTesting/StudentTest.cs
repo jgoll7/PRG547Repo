62cedcdf9b79ad88962e00a7ccd6aca5699f9e31
jacks <jackson.GOLLAN@student.tafesa.edu.au>
2023-11-29 10:37:20 +1030

    Created Console Project to test library. Installed NUnit Framework, and wrote equality test for Student objects.

---
+++ TafeSA Enrolment System/LibraryTesting/StudentTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TafeSAEnrolmentLibrary
+{
+    [TestFixture]
+    class StudentTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void TestStudent()
+        {
+            Student s1 = new Student(1, "Mark", "Mark@mail.com", "04940930", "Cert IV Programming", new DateTime());
+            Student s2 = new Student(1, "Mark", "Mark@mail.com", "04940930", "Cert IV Programming", new DateTime());
+            Assert.That(s1 != s2);
+        }
+        
+    }
+}
